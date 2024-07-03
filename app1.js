06ec09e0e16d9e7dc458cb3dd88dec079877aa4d
Sample-niha <115917218+Sample-niha@users.noreply.github.com>
2024-07-03 14:35:56 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,58 @@
+const Base_url="https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn=document.querySelector("form button");
+const fromCurr =document.querySelector(".from select");
+const toCurr = document.querySelector(".To select");
+const msg=document.querySelector(".msg");
+for(let select of dropdowns)
+ {
+    for(currCode in countryList)
+    {
+       let newOption=document.createElement("option");
+       newOption.innerText=currCode;
+       newOption.value=currCode;
+       if(select.name ==="from" && currCode==="USD")
+        {
+            newOption.selected="selected";
+        }
+        else if(select.name === "To" && currCode ==="INR")
+        {
+          newOption.selected="selected";   
+        }
+       select.append(newOption);
+    }
+select.addEventListener("change",(evt) =>{
+    updateFlag(evt.target);
+});
+}
+const updateFlag =(element) =>{
+   let currCode = element.value;
+   let countryCode = countryList[currCode];
+   let newSrc =`https://flagsapi.com/${countryCode}/shiny/64.png`;
+   let img=element.parentElement.querySelector("img");
+   img.src=newSrc;
+};
+btn.addEventListener("click", async (evt) =>{
+    evt.preventDefault();
+    let amount=document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if(amtVal === "" || amtVal < 1)
+        {
+            amtVal = 1;
+            amount.value="1";
+        }
+        console.log(fromCurr.value,toCurr.value);
+     const Url=`${Base_url}/${fromCurr.value.toLowerCase()}.min.json`;
+    let response = await fetch(Url);
+    let data = await response.json();
+    let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+    console.log(rate);
+    console.log(amtVal);
+    let finalAmount = amtVal * rate;
+    msg.innerText=`${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+});
+
+fetch("https://api.github.com/orgs/nodejs")
+.then(response => response.json())
+.then(jsonData => console.log(jsonData))
+
